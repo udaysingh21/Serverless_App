9b3c012e1b9d4a8bac2ff5ea9e24447c87dfc655
Uday Vikram Singh <uday.vikram.singh@sap.com>
2025-02-20 00:01:12 +0530

    Add Jest tests and CI test step

--- __tests__/handler.test.js
+++ __tests__/handler.test.js
@@ -4,12 +4,12 @@
 const { hello } = require('../handler');
 
 describe('hello function', () => {
-  it('should return a statusCode of 200 and "Hello,  world!" message', async () => {
+  it('should return a statusCode of 200 and "Hello, DevOps world!" message', async () => {
     const result = await hello();
 
     expect(result.statusCode).toBe(200);
 
     const body = JSON.parse(result.body);
-    expect(body.message).toBe('Hello, world!');
+    expect(body.message).toBe('Hello, DevOps world!');
   });
 });
