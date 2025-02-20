5d280ae1a22e7de0ddc5faa98a352cb0a2047088
Uday Vikram Singh <uday.vikram.singh@sap.com>
2025-02-20 11:07:26 +0530

    Fixed test

--- __tests__/handler.test.js
+++ __tests__/handler.test.js
@@ -1,11 +1,11 @@
 // __tests__/handler.test.js
-const { hello } = require('../handler') // CommonJS require
+const { hello } = require("../handler"); // CommonJS require
 
-describe('hello function', () => {
+describe("hello function", () => {
   it('should return a statusCode of 200 and "Hello, DevOps world!" message', async () => {
-    const result = await hello()
-    expect(result.statusCode).toBe(200)
-    const body = JSON.parse(result.body)
-    expect(body.message).toBe('Hello, DevOps world!')
-  })
-})
+    const result = await hello();
+    expect(result.statusCode).toBe(200);
+    const body = JSON.parse(result.body);
+    expect(body.message).toBe("Hello, DevOps world!");
+  });
+});
--- handler.js
+++ handler.js
@@ -1,7 +1,6 @@
 module.exports.hello = async () => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello, DevOps world!" }),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello, DevOps world!" }),
   };
-  
+};
